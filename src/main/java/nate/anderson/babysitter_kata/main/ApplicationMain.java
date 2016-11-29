d7ce2d4cdf45deb36dd0cfb77ff8d10d7d1d54bf
Nate Anderson <nathanaelcanderson@gmail.com>
2016-11-28 21:12:46 -0500

    add application main, need to debug iocontroller

---
+++ src/main/java/nate/anderson/babysitter_kata/main/ApplicationMain.java
@@ -0,0 +1,88 @@
+package nate.anderson.babysitter_kata.main;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+import javax.naming.directory.InvalidAttributesException;
+
+import nate.anderson.babysitter_kata.controller.IOController;
+import nate.anderson.babysitter_kata.controller.RatesController;
+import nate.anderson.babysitter_kata.model.Rates;
+import nate.anderson.babysitter_kata.model.SittingShift;
+
+public class ApplicationMain {
+	
+	Rates rates;
+	SittingShift sittingShift;
+	IOController ioController;
+	RatesController ratesController;
+	
+	Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		ApplicationMain applicationMain = new ApplicationMain();
+		applicationMain.run();
+	}
+	
+	public void run() {
+		rates = new Rates();
+		sittingShift = new SittingShift();
+		ioController = new IOController();
+		
+		while (true) {
+			
+			LocalTime formattedTime;
+			String input;
+			
+			System.out.println("Please enter shift start time: ");
+			try {
+				input = scanner.nextLine();
+				formattedTime = ioController.formatTime(input);
+				ioController.addTimeToList(formattedTime);
+			}
+			catch (DateTimeParseException e) {
+				System.out.println("Please enter a time in the format 'HH:mm am/pm'");
+				e.printStackTrace();
+			}
+			
+			System.out.println("Please enter bedtime: ");
+			try {
+				input = scanner.nextLine();
+				formattedTime = ioController.formatTime(input);
+				ioController.addTimeToList(formattedTime);
+			}
+			catch (DateTimeParseException e) {
+				System.out.println("Please enter a time in the format 'HH:mm am/pm'");
+				e.printStackTrace();
+			}
+			
+			System.out.println("Please enter shift end time: ");
+			try {
+				input = scanner.nextLine();
+				formattedTime = ioController.formatTime(input);
+				ioController.addTimeToList(formattedTime);
+			}
+			catch (DateTimeParseException e) {
+				System.out.println("Please enter a time in the format 'HH:mm am/pm'");
+				e.printStackTrace();
+			}
+		
+		
+			try {
+				ioController.assignTimes(sittingShift, ioController.getListOfTimes());
+				ratesController = new RatesController(sittingShift, rates);
+				break;
+			} 
+			catch (InvalidAttributesException e) {
+				System.out.println("One or more times are invalid, please try again");
+				e.printStackTrace();
+			}
+			
+		}
+		
+		System.out.println(ratesController.calculateCost());
+		
+	}
+
+}
